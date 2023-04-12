daeef1e0b8ac9735fb3689945f19002d32d0071e
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2023-04-13 06:04:52 +0700

    add view engine

--- server.js
+++ server.js
@@ -3,15 +3,47 @@ const { getUsers } = require('./helper');
 
 const server = express();
 
+server.set('view engine', 'ejs');
+server.set('views', 'public');
+
 server.use(express.json());
 server.use('/static', express.static('public/static'));
 
 server.get('/', function(req, res) {
-    res.sendFile(`public/chapter3.html`, { root: __dirname });
+    // res.sendFile(`public/chapter3.html`, { root: __dirname });
+    res.render(`chapter3.ejs`, {
+        headerText: 'PLAY TRADITIONAL GAME',
+        headerSubText: 'Experience new traditional game play',
+    });
 });
 
 server.get('/game', function(req, res) {
     res.sendFile(`public/chapter4.html`, { root: __dirname });
+});
+
+server.get('/test', function(req, res) {
+    const { name } = req.query;
+    const users = getUsers();
+
+    const result = users.map(function(user) {
+        return {
+            username: user.username,
+        }
+    });
+    
+    res.render('test.ejs', {
+        name,
+        lain: 'non abstrak',
+        users: result,
+    });
+    // res.sendFile(`public/test.html`, { root: __dirname });
+    // res.send(`
+    //     <html>
+    //         <body>
+    //             <h1>Hello, ${name}</h1>
+    //         </body>
+    //     </html>
+    // `);
 });
 
 server.get('/api/v1/accounts', function(req, res) {
